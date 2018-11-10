d9c1ba2c65618ea6b54e4da901d6a42793685270
Adomas Poleninas <adomas.poleninas@gmail.com>
2018-11-10 13:56:03 +0200

    Filtering now is working

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,70 @@
 import React from 'react';
 import Card from './Card';
 import axios from 'axios';
-import { endpoints } from '../../config';
+import {endpoints} from '../../config';
 
 class App extends React.Component {
-  constructor() {
-    super();
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      movieList: [],
+        this.state = {
+            movieList: [],
+            genreList: [],
+        }
     }
-  }
 
-  componentDidMount() {
-    this.requestPopularMovies();
-  }
+    componentDidMount() {
+        this.requestPopularMovies();
+        this.requestGenres();
+    }
 
-  requestPopularMovies = () => {
-    axios
-      .get(endpoints.mostPopularMovies())
-      .then((response) => {
-        this.setState({
-          movieList: response.data.results,
-        });
-      })
-      .catch((error) => console.log(error.response));
-  };
+    requestPopularMovies = () => {
+        axios
+            .get(endpoints.mostPopularMovies())
+            .then((response) => {
+                this.setState({
+                    movieList: response.data.results,
+                });
+            })
+            .catch((error) => console.log(error.response));
+    };
 
-  render() {
-    const { movieList } = this.state;
+    requestGenres = () => {
+        axios
+            .get(endpoints.genres())
+            .then((response) => {
+                this.setState({
+                    genreList: response.data.genres,
+                });
+            })
+            .catch((error) => console.log(error.response));
+    };
 
-    return (
-      <React.Fragment>
-        {movieList.map((movie) => (
-          <Card key={movie.id} data={movie} />
-        ))}
-      </React.Fragment>
-    );
-  }
+    filterGenreMovies = (id) => {
+        axios
+            .get(endpoints.genreMovies(id))
+            .then((response) => {
+                this.setState({
+                    movieList: response.data.results,
+                });
+            })
+            .catch((error) => console.log(error.response));
+    };
+
+    render() {
+        const {genreList, movieList} = this.state;
+
+        return (
+            <React.Fragment>
+                {genreList.map((genre) => (
+                    <button onClick={(() => {this.filterGenreMovies(genre.id)})}>{genre.name}</button>
+                ))}
+                {movieList.map((movie) => (
+                    <Card key={movie.id} data={movie}/>
+                ))}
+            </React.Fragment>
+        );
+    }
 }
 
 export default App;
