8c593642bdb9ba7fd32de2ae233fcf6cb37d4722
Adomas Poleninas <adomas.poleninas@gmail.com>
2018-11-11 17:38:40 +0200

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,34 @@ import axios from 'axios';
 import {endpoints} from '../../config';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+    constructor() {
+        super();
 
         this.state = {
             movieList: [],
             genreList: [],
+            isLiked: [],
         }
     }
+
+    onHeartClick = (id) => {
+        const likedArray = this.state.isLiked;
+
+        if (likedArray.length !== 0) {
+
+            likedArray.map(favouriteId => {
+                if (favouriteId === id) {
+                    likedArray.splice(likedArray.indexOf(id), 1);
+                } else {
+                    likedArray.push(id);
+                }
+            });
+        } else {
+            likedArray.push(id);
+        }
+
+        this.setState({liked: likedArray});
+    };
 
     componentDidMount() {
         this.requestPopularMovies();
@@ -52,23 +72,30 @@ class App extends React.Component {
     };
 
     render() {
-        const {genreList, movieList} = this.state;
+        const {genreList, movieList, isLiked} = this.state;
 
         return (
             <React.Fragment>
-                {genreList.map((genre, index) => {
-                    return (
-                        <button
-                            onClick={(() => {
-                                this.filterGenreMovies(genre.id)
-                            })}
-                            key={index}>
-                            {genre.name}
-                        </button>
-                    );
-                })}
+                <div className="genres">
+                    {genreList.map((genre) => {
+                        return (
+                            <a className="genre"
+                               onClick={(() => {
+                                   this.filterGenreMovies(genre.id)
+                               })}
+                               key={genre.id}>
+                                {genre.name}
+                            </a>
+                        );
+                    })}
+                </div>
                 {movieList.map((movie) => (
-                    <Card key={movie.id} data={movie}/>
+                    <Card onClick={() => this.onHeartClick(movie.id)}
+                          isLiked={isLiked}
+                          key={movie.id}
+                          id={movie.id}
+                          data={movie}
+                    />
                 ))}
             </React.Fragment>
         );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,70 +1,91 @@
 import React from 'react';
-import { getImageUrl } from '../../config';
+import {getImageUrl} from '../../config';
+import PropTypes from 'prop-types';
 
 class Card extends React.Component {
-  constructor() {
-    super();
 
-    this.state = {
-      summaryShowing: false,
+    constructor() {
+        super();
+
+        this.state = {
+            summaryShowing: false,
+        };
+    }
+
+    toggleSummary = () => {
+        const {summaryShowing} = this.state;
+
+        this.setState({
+            summaryShowing: !summaryShowing,
+        });
     };
-  }
 
-  toggleSummary = () => {
-    const { summaryShowing } = this.state;
+    hasHeart = (isLiked, id) => {
+        let heart = '-o';
 
-    this.setState({
-      summaryShowing: !summaryShowing,
-    });
-  };
+        isLiked.map(movieId => {
 
-  render() {
-    const { summaryShowing } = this.state;
-    const {
-      data: {
-        poster_path,
-        original_title,
-        overview,
-        release_date,
-        vote_average,
-        vote_count,
-      },
-    } = this.props;
+            if (movieId === id) {
+                heart = '';
+            }
 
-    return (
-      <div className="card">
-        <div className="card__image" style={{ backgroundImage: `url(${getImageUrl(poster_path)})` }} />
+        });
+        return heart;
+    };
 
-        <div className="card__title">
-          {original_title}
-        </div>
+    render() {
+        const {summaryShowing} = this.state;
+        const {
+            data: {
+                poster_path,
+                original_title,
+                overview,
+                release_date,
+                vote_average,
+                vote_count,
+            },
+        } = this.props;
 
-        <div className="card__like">
-          <i className="fa fa-heart-o" />
-        </div>
+        return (
+            <div className="card">
+                <div className="card__image" style={{backgroundImage: `url(${getImageUrl(poster_path)})`}}/>
 
-        <div className="card__subtitle">
-          <span>{release_date}</span>
-          <span>{vote_average} ({vote_count} votes)</span>
-        </div>
+                <div className="card__title">
+                    {original_title}
+                </div>
 
-        {summaryShowing
-          ? (
-            <div className="card-info">
-              <div className="card-info__header">Summary</div>
-              <div className="card-info__description">
-                {overview}
-              </div>
+                <div onClick={this.props.onClick} className="card__like">
+                    <i className={`fa fa-heart${this.hasHeart(this.props.isLiked, this.props.id)}`}/>
+                </div>
+
+                <div className="card__subtitle">
+                    <span>{release_date}</span>
+                    <span>{vote_average} ({vote_count} votes)</span>
+                </div>
+
+                {summaryShowing
+                    ? (
+                        <div className="card-info">
+                            <div className="card-info__header">Summary</div>
+                            <div className="card-info__description">
+                                {overview}
+                            </div>
+                        </div>
+                    )
+                    : null
+                }
+
+                <div className="button" onClick={() => this.toggleSummary()}>Show summary</div>
             </div>
-          )
-          : null
-        }
 
-        <div className="button" onClick={() => this.toggleSummary()}>Show summary</div>
-      </div>
-
-    );
-  }
+        );
+    }
 }
+
+Card.propTypes = {
+    isLiked: PropTypes.array,
+    // movieId: PropTypes.number,
+};
+
 
 export default Card;
